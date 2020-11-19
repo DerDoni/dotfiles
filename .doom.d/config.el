38195a630f79b470fda3555924156a7d84ebe037
Vincenzo Pace <vincenzo.pace@student.kit.edu>
2020-11-19 10:48:57 +0100

    initial commit

---
+++ .doom.d/config.el
@@ -0,0 +1,189 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+(map! :leader
+      :desc "List bookmarks"
+      "b L" #'list-bookmarks
+      :leader
+      :desc "Save current bookmarks to bookmark file"
+      "b w" #'bookmark-save)
+
+
+(map! :leader
+      :desc "Dired"
+      "d d" #'dired
+      :leader
+      :desc "Dired jump to current"
+      "d j" #'dired-jump
+      (:after dired
+        (:map dired-mode-map
+         :leader
+         :desc "Peep-dired image previews"
+         "d p" #'peep-dired
+         :leader
+         :desc "Dired view file"
+         "d v" #'dired-view-file)))
+;; Make 'h' and 'l' go back and forward in dired. Much faster to navigate the directory structure!
+(evil-define-key 'normal dired-mode-map
+  (kbd "h") 'dired-up-directory
+  (kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
+;; If peep-dired is enabled, you will get image previews as you go up/down with 'j' and 'k'
+(evil-define-key 'normal peep-dired-mode-map
+  (kbd "j") 'peep-dired-next-file
+  (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+;; Get file icons in dired
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+;; With dired-open plugin, you can launch external programs for certain extensions
+;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
+(setq dired-open-extensions '(("gif" . "sxiv")
+                              ("jpg" . "sxiv")
+                              ("png" . "sxiv")
+                              ("mkv" . "mpv")
+                              ("mp4" . "mpv")))
+
+
+
+(setq user-full-name "Vincenzo Pace"
+      user-mail-address "vincenzo.pace94@icloud.com"
+      display-line-numbers-type nil
+      confirm-kill-emacs nil)
+
+(after! company
+  (setq company-idle-delay 0.5
+        company-minimum-prefix-length 2)
+  (setq company-show-numbers t)
+  (add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less
+
+(setq-default history-length 1000)
+(setq-default prescient-history-length 1000)
+
+
+(setq doom-font (font-spec :family "Iosevka" :size 16)
+      doom-variable-pitch-font (font-spec :family "Libre Baskerville")
+      doom-serif-font (font-spec :family "Libre Baskerville"))
+
+
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(after! org
+  (require 'org-bullets)  ; Nicer bullets in org-mode
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (setq org-directory "~/Org/"
+        org-agenda-files '("~/Org/agenda.org")
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-log-done 'time
+        org-journal-dir "~/Org/journal/"
+        org-journal-date-format "%B %d, %Y (%A)"
+        org-journal-file-format "%Y-%m-%d.org"
+        org-hide-emphasis-markers t
+        ;; ex. of org-link-abbrev-alist in action
+        ;; [[arch-wiki:Name_of_Page][Description]]
+        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
+          '(("google" . "http://www.google.com/search?q=")
+            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
+            ("ddg" . "https://duckduckgo.com/?q=")
+            ("wiki" . "https://en.wikipedia.org/wiki/"))
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence
+             "TODO(t)"
+             "RESEARCH(r)"
+             "PROJ(p)"
+             "WAIT(w)"
+             "|"
+             "DONE(d)"
+             "CANCELLED(c)" ))))
+
+
+
+(use-package! notmuch
+  :commands (notmuch)
+  :init
+;;  (map! :desc "notmuch" "<f2>" #'notmuch)
+  (map! :map notmuch-search-mode-map
+        :desc "toggle read" "t" #'+notmuch/toggle-read
+        :desc "Reply to thread" "r" #'notmuch-search-reply-to-thread
+        :desc "Reply to thread sender" "R" #'notmuch-search-reply-to-thread-sender
+        :desc "Filter" "/" #'notmuch-search-filter
+        :desc "Archive All" "A" #'+notmuch-archive-all
+        :desc "Delete All" "D" #'+notmuch-delete-all)
+  (map! :map notmuch-show-mode-map
+        :desc "Next link" "<tab>" #'org-next-link
+        :desc "Previous link" "<backtab>" #'org-previous-link
+        :desc "URL at point" "C-<return>" #'browse-url-at-point)
+  (defun +notmuch/toggle-read ()
+    "toggle read status of message"
+    (interactive)
+    (if (member "unread" (notmuch-search-get-tags))
+        (notmuch-search-tag (list "-unread"))
+      (notmuch-search-tag (list "+unread"))))
+  :config
+  (setq message-auto-save-directory "~/.mail/drafts/"
+        message-send-mail-function 'message-send-mail-with-sendmail
+        sendmail-program (executable-find "msmtp")
+        message-sendmail-envelope-from 'header
+        mail-envelope-from 'header
+        mail-specify-envelope-from t
+        message-sendmail-f-is-evil nil
+        message-kill-buffer-on-exit t
+        notmuch-always-prompt-for-sender t
+        notmuch-archive-tags '("-unread")
+        notmuch-crypto-process-mime t
+        notmuch-hello-sections '(notmuch-hello-insert-saved-searches)
+        notmuch-labeler-hide-known-labels t
+        notmuch-search-oldest-first nil
+        notmuch-archive-tags '("-inbox" "-unread")
+        notmuch-message-headers '("To" "Cc" "Subject" "Bcc")
+        notmuch-saved-searches '((:name "inbox" :query "tag:inbox")
+                                 (:name "unread" :query "tag:inbox and tag:unread")
+                                 (:name "to-me" :query "tag:inbox and tag:to-me")
+                                 (:name "personal" :query "tag:inbox and tag:personal")
+                                 (:name "drafts" :query "tag:draft")))
+
+  (defun +notmuch-archive-all ()
+    "Archive all the emails in the current view."
+    (interactive)
+    (notmuch-search-archive-thread nil (point-min) (point-max)))
+
+
+  (defun +notmuch-delete-all ()
+    "Archive all the emails in the current view.
+Mark them for deletion by cron job."
+    (interactive)
+    (notmuch-search-tag-all '("+deleted"))
+    (+notmuch-archive-all)))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
