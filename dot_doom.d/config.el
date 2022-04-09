0ee9c73b0aa2bc85f3bae637deb658ae6ceff0f5
Vincenzo Pace <vincenzo.pace@mailbox.org>
2022-04-09 18:50:06 +0200

    initial commit

---
+++ dot_doom.d/config.el
@@ -0,0 +1,321 @@
+;; [[file:config.org::*Defaults][Defaults:1]]
+(setq user-full-name "Vincenzo Pace"
+      user-mail-address "vincenzo.pace@mailbox.org")
+
+(setq-default
+      delete-by-moving-to-trash t
+      window-combination-resize t
+      x-stretch-cursor t
+      major-mode 'org-mode
+      history-length 1000
+      prescient-history-length 1000)
+
+(setq undo-limit 800000000
+      evil-want-fine-undo t
+      truncate-string-ellipsis "…"
+      password-cache-expiry nil
+      doom-fallback-buffer-name "► Doom"
+      +doom-dashboard-name "► Doom")
+
+(display-time-mode 1)
+;; Defaults:1 ends here
+
+;; [[file:config.org::*Company][Company:1]]
+(after! company
+  (setq company-idle-delay 0.2
+        company-minimum-prefix-length 2
+        company-show-quick-access t
+        )
+  (add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less
+;; Company:1 ends here
+
+;; [[file:config.org::*Theming][Theming:1]]
+(setq doom-theme 'modus-vivendi)
+(remove-hook 'window-setup-hook #'doom-init-theme-h)
+(add-hook 'after-init-hook #'doom-init-theme-h 'append)
+(delq! t custom-theme-load-path)
+
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+
+
+(setq display-line-numbers-type 'relative
+      confirm-kill-emacs nil)
+
+(use-package modus-themes
+  :ensure
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+        modus-themes-completions '((matches . (extrabold))
+                                  (selection . (semibold accented))
+                                  (popup . (accented intense)))
+        modus-themes-variable-pitch-headings t
+        modus-themes-scale-headings t
+        modus-themes-variable-pitch-ui t
+        modus-themes-org-agenda
+        '((header-block . (variable-pitch scale-title))
+          (header-date . (grayscale bold-all)))
+        modus-themes-org-blocks
+        '(grayscale)
+        modus-themes-mode-line
+        '(borderless)
+        modus-themes-region '(bg-only no-extend))
+
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  :config
+  (modus-themes-load-operandi)
+  :bind ("<f5>" . modus-themes-toggle))
+;; Theming:1 ends here
+
+;; [[file:config.org::*Modeline][Modeline:1]]
+(defun doom-modeline-conditional-buffer-encoding ()
+  "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
+  (setq-local doom-modeline-buffer-encoding
+              (unless (and (memq (plist-get (coding-system-plist buffer-file-coding-system) :category)
+                                 '(coding-category-undecided coding-category-utf-8))
+                           (not (memq (coding-system-eol-type buffer-file-coding-system) '(1 2))))
+                t)))
+
+(add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding)
+;; Modeline:1 ends here
+
+;; [[file:config.org::*Windows][Windows:1]]
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (consult-buffer))
+
+(map! :map evil-window-map
+      "SPC" #'rotate-layout
+      ;; Navigation
+      "<left>"     #'evil-window-left
+      "<down>"     #'evil-window-down
+      "<up>"       #'evil-window-up
+      "<right>"    #'evil-window-right
+      ;; Swapping windows
+      "C-<left>"       #'+evil/window-move-left
+      "C-<down>"       #'+evil/window-move-down
+      "C-<up>"         #'+evil/window-move-up
+      "C-<right>"      #'+evil/window-move-right)
+;; Windows:1 ends here
+
+;; [[file:config.org::*General Settings][General Settings:1]]
+(setq org-directory "~/org/")
+(after! org
+  (require 'org-bullets)
+  (require 'org-habit)
+  :config
+  (setq org-startup-folded t
+        org-preview-latex-directory (expand-file-name "ltximg/" org-directory)
+        org-habit-show-habits t
+        org-agenda-files '("~/org/todo.org" "~/org/habits.org" )
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-my-anki-file (expand-file-name "anki.org" org-directory)
+        org-log-done 'time
+        org-journal-dir "~/org/journal/"
+        org-journal-date-format "%B %d, %Y (%A)"
+        org-journal-file-format "%Y-%m-%d.org"
+        org-hide-emphasis-markers t
+        org-pomodoro-manual-break t)
+  )
+;; General Settings:1 ends here
+
+;; [[file:config.org::*Org Download][Org Download:1]]
+(use-package! org-download
+  :commands
+  org-download-dnd
+  org-download-yank
+  org-download-screenshot
+  org-download-dnd-base64
+  :init
+  (map! :map org-mode-map
+        "s-Y" #'org-download-screenshot
+        "s-y" #'org-download-yank)
+  (pushnew! dnd-protocol-alist
+            '("^\\(?:https?\\|ftp\\|file\\|nfs\\):" . org-download-dnd)
+            '("^data:" . org-download-dnd-base64))
+  (advice-add #'org-download-enable :override #'ignore)
+  :config
+  (defun +org/org-download-method (link)
+    (let* ((filename
+            (file-name-nondirectory
+             (car (url-path-and-query
+                   (url-generic-parse-url link)))))
+           ;; Create folder name with current buffer name, and place in root dir
+           (dirname (concat "./images/"
+                            (replace-regexp-in-string " " "_"
+                                                      (downcase (file-name-base buffer-file-name))))))
+      (make-directory dirname t)
+      (expand-file-name filename dirname)))
+  :config
+  ;; org-attach method
+(setq-default org-attach-method 'mv
+              org-attach-auto-tag "attach"
+              org-attach-store-link-p 't)
+(setq-default org-download-method 'directory
+              org-download-image-dir "~/org/screenshots/"
+              org-download-heading-lvl nil
+              org-download-delete-image-after-download t
+              org-download-screenshot-method "flameshot gui --raw > %s"
+              org-download-image-org-width 300
+              org-download-annotate-function (lambda (link) "") ;; Don't annotate
+              )
+:config
+  (defun +org/org-download-method (link)
+    (let* ((filename
+            (file-name-nondirectory
+             (car (url-path-and-query
+                   (url-generic-parse-url link)))))
+           ;; Create folder name with current buffer name, and place in root dir
+           (dirname (concat "./images/"
+                            (replace-regexp-in-string " " "_"
+                                                      (downcase (file-name-base buffer-file-name))))))
+      (make-directory dirname t)
+      (expand-file-name filename dirname)))
+:config
+(setq org-download-screenshot-method
+        (cond (IS-MAC "screencapture -i %s")
+              (IS-LINUX
+               (cond ((executable-find "maim")  "maim -u -s %s")
+                     ((executable-find "scrot") "scrot -s %s")))))
+  (setq org-download-method '+org/org-download-method))
+;; Org Download:1 ends here
+
+;; [[file:config.org::*Org Capture][Org Capture:1]]
+(use-package! anki-editor
+  :commands (anki-editor-mode)
+  :init
+  (map! :leader
+      :desc "Anki Push tree"
+      "m a p" #'anki-editor-push-tree)
+  :hook (org-capture-after-finalize . anki-editor-reset-cloze-number) ; Reset cloze-number after each capture.
+  :config
+  (setq anki-editor-create-decks t ;; Allow anki-editor to create a new deck if it doesn't exist
+        anki-editor-org-tags-as-anki-tags t
+        anki-editor-break-consecutive-braces-in-latex t)
+
+  (defun anki-editor-cloze-region-auto-incr (&optional arg)
+    "Cloze region without hint and increase card number."
+    (interactive)
+    (anki-editor-cloze-region my-anki-editor-cloze-number "")
+    (setq my-anki-editor-cloze-number (1+ my-anki-editor-cloze-number))
+    (forward-sexp))
+  (defun anki-editor-cloze-region-dont-incr (&optional arg)
+    "Cloze region without hint using the previous card number."
+    (interactive)
+    (anki-editor-cloze-region (1- my-anki-editor-cloze-number) "")
+    (forward-sexp))
+  (defun anki-editor-reset-cloze-number (&optional arg)
+    "Reset cloze number to ARG or 1"
+    (interactive)
+    (setq my-anki-editor-cloze-number (or arg 1)))
+  (defun anki-editor-push-tree ()
+    "Push all notes under a tree."
+    (interactive)
+    (anki-editor-push-notes '(4))
+    (anki-editor-reset-cloze-number))
+  ;; Initialize
+  (anki-editor-reset-cloze-number)
+  )
+
+
+;; Org-capture templates
+(setq org-my-anki-file "~/org/anki.org")
+(after! org
+    (add-to-list 'org-capture-templates
+    '("a" "Anki basic"
+               entry
+               (file+headline org-my-anki-file "Dispatch Shelf")
+               "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Mega\n:END:\n** Front\n%?\n** Back\n"))
+    (add-to-list 'org-capture-templates
+             '("A" "Anki cloze"
+               entry
+               (file+headline org-my-anki-file "Dispatch Shelf")
+               "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Cloze\n:ANKI_DECK: Mega\n:END:\n** Text\n%x\n** Extra\n"))
+    (add-to-list 'org-capture-templates
+                '("g" "Game Dev Notes"
+                  entry
+                  (file+headline "~/org/my_rpg.org" "Capture")
+                   "* %?\nEntered on %U\n  %i\n  %a"))
+    (add-to-list 'org-capture-templates
+                '("r" "Reading List"
+                  entry
+                  (file+headline "~/org/reading_list.org" "Capture")
+                   "* %?Title\nby Author \n\nEntered on %U\n  %i\n  %a \n ")))
+
+;; Allow Emacs to access content from clipboard.
+(setq select-enable-clipboard t
+      select-enable-primary t)
+
+(defadvice org-capture-finalize
+    (after delete-capture-frame activate)
+  "Advise capture-finalize to close the frame"
+  (if (equal "org-capture" (frame-parameter nil 'name))
+      (delete-frame)))
+
+(defadvice org-capture-destroy
+    (after delete-capture-frame activate)
+  "Advise capture-destroy to close the frame"
+  (if (equal "org-capture" (frame-parameter nil 'name))
+      (delete-frame)))
+
+(defun make-orgcapture-frame ()
+    "Create a new frame and run org-capture."
+    (interactive)
+    (make-frame '((name . "org-capture") (window-system . x)))
+    (select-frame-by-name "org-capture")
+    (org-capture)
+    ;;(delete-other-windows)
+    )
+;; Org Capture:1 ends here
+
+;; [[file:config.org::*Org Roam][Org Roam:1]]
+(use-package! org-roam
+:config
+ (setq org-roam-capture-templates
+        '(("m" "main" plain
+           "%?"
+           :if-new (file+head "main/${slug}.org"
+                              "#+title: ${title}\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("r" "reference" plain "%?"
+           :if-new
+           (file+head "reference/${slug}.org" "#+title: ${title}\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("a" "article" plain "%?"
+           :if-new
+           (file+head "articles/${slug}.org" "#+title: ${title}\n#+filetags: :article:\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("b" "book notes" plain
+           "\n* Source\n\nAuthor: %^{Author}\nTitle: ${title}\nYear: %^{Year}\n\n* Summary\n\n%?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("d" "default" plain
+           "%?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t))))
+;; Org Roam:1 ends here
+
+;; [[file:config.org::*Mathpix][Mathpix:1]]
+(use-package! mathpix.el
+  :commands (mathpix-screenshot)
+  :init
+  (map! "C-x m" #'mathpix-screenshot)
+  :config
+  (setq mathpix-screenshot-method "xfce4-screenshooter -r -o cat > %s"
+        mathpix-app-id (with-temp-buffer (insert-file-contents "./secrets/mathpix-app-id") (buffer-string))
+        mathpix-app-key (with-temp-buffer (insert-file-contents "./secrets/mathpix-app-key") (buffer-string))))
+;; Mathpix:1 ends here
+
+;; [[file:config.org::*Elfeed][Elfeed:1]]
+(add-hook! 'elfeed-search-mode-hook #'elfeed-update)
+;; Elfeed:1 ends here
---
+++ dot_doom.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://this-week-in-rust.org/rss.xml"
+     ("http://feeds.bbci.co.uk/news/rss.xml" news tops)
+     ("https://this-week-in-rust.org/rss.xml" programming rust)))
+ '(smtpmail-smtp-server "smtp.mailbox.org")
+ '(smtpmail-smtp-service 25)
+ '(warning-suppress-types '(((undo discard-info)) ((undo discard-info)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
